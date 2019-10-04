74f6620bea87aab4f2b052c5352e6ef2514f736b
venkatavikas69 <venkatavikas69@gmail.com>
2019-10-04 19:59:30 +0530

    Create num.js

---
+++ Tuesday/Numbers,strings and booleans/num.js
@@ -0,0 +1,3 @@
+/* strings are the set of characters that stored in a computer in the form of bits*/
+/*booleans are the values that are indicated by either 1 and 0 or true and false, boolean values tells us only the given condition or a specific
+value is correct or incorrect*/ 
