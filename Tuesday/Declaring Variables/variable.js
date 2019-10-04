a9b0818c15a2face2ba3251010c2c964014cae0a
venkatavikas69 <venkatavikas69@gmail.com>
2019-10-04 20:32:52 +0530

    Update variable.js

--- Tuesday/Declaring Variables/variable.js
+++ Tuesday/Declaring Variables/variable.js
@@ -1,1 +1,11 @@
 /* this an exercise of declaring variables in javascript*/
+/* In javascript we use variables to store data that can be manipulated at any time. variables are like an empty boxes 
+that are labelled with names and an empty space inside of them to store something and that items in the box can be replaced
+at any time*/
+/*we use "var" keyword to create a variabe in javascript and we can store any values like string,numbers ,boolean,float,null or 
+we can keep it empty*/
+var number=10;
+var strings=vikas;
+var boolean=true;
+var empty;
+var keepnull=null;
